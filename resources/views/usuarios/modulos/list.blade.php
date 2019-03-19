6ed7f206ed016e5885b7e3edbd0118336a460b99
Jeisson Mandon <jorgejeisson@hotmail.com>
2019-03-19 15:17:41 -0500

    Versión 1.4 19 de Marzo 2019 15:16 Jeisson BETA

---
+++ resources/views/usuarios/modulos/list.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.admin')
+@section('breadcrumb')
+<h1>
+    Bienvenido
+    <small>Sr(a). {{Auth::user()->nombres}}</small>
+</h1>
+<ol class="breadcrumb">
+    <li><a href="{{route('home')}}"><i class="fa fa-home"></i> Inicio</a></li>
+    <li><a href="{{route('menu.usuarios')}}"><i class="fa fa-users"></i> Usuarios</a></li>
+    <li class="active"><a>Listado</a></li>
+</ol>
+@endsection
+@section('content')
+<div class="box">
+    <div class="box-header with-border">
+        <h3 class="box-title">LISTADO DE USUARIOS</h3>
+        <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-toggle="modal" data-target="#modal" title="Ayuda">
+                <i class="fa fa-question"></i></button>
+            <a class="btn btn-box-tool" data-toggle="tooltip" data-original-title="Agregar Módulo">
+                <i class="fa fa-plus-circle"></i></a>
+            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Minimizar">
+                <i class="fa fa-minus"></i></button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Cerrar">
+                <i class="fa fa-times"></i></button>
+        </div>
+    </div>
+    <div class="box-body">
+
+    </div>
+</div>
+<div class="modal fade" id="modal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Información de Ayuda</h4>
+            </div>
+            <div class="modal-body">
+                <p>Los módulos generales del sistema son las aplicaciones generales representadas en las opciones del menú. Ejemplo de modulo general: ACADÉMICO ESTUDIANTE.</p>
+                <p><b>Nota:</b> No modifique los nombres de los módulos ya creados ya que puede ocasionar fallas en el sistema.</p>
+            </div>
+            <div class="modal-footer" style="background-color: #d2d6de !important; opacity: .65;">
+                <button type="button"  class="btn btn-block btn-danger btn-flat pull-right" data-dismiss="modal"> <i class="fa fa-reply"></i> Regresar</button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+@endsection
