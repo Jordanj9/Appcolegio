9f9d63fbfefe92832b74bb6427d1f1d2767ed45c
johnp <johnp@192.168.0.14>
2019-04-30 16:52:52 -0500

    Version 9.1 30 de Abril de 2019 16:52 johnp Beta

---
+++ resources/views/matricula/datos_basicos/categoria/create.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.admin')
+@section('breadcrumb')
+<h1>
+    Bienvenido
+    <small>Sr(a). {{Auth::user()->nombres}}</small>
+</h1>
+<ol class="breadcrumb">
+    <li><a href="{{route('home')}}"><i class="fa fa-home"></i> Inicio</a></li>
+    <li><a href="{{route('menu.matricula')}}"><i class="fa fa-tasks"></i> Matrícula</a></li>
+    <li><a href="{{route('menu.matricula')}}"><i class="fa fa-cogs"></i> Datos Básicos</a></li>
+    <li><a href="{{route('categoria.index')}}"><i class="fa fa-list-alt"></i> Categoría</a></li>
+    <li class="active"><a>Crear</a></li>
+</ol>
+@endsection
+@section('content')
+<div class="box">
+    <div class="box-header with-border">
+        <h3 class="box-title">CREAR NUEVA CATEGORÍA</h3>
+        <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-toggle="modal" data-target="#modal" title="Ayuda">
+                <i class="fa fa-question"></i></button>
+            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Minimizar">
+                <i class="fa fa-minus"></i></button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Cerrar">
+                <i class="fa fa-times"></i></button>
+        </div>
+    </div>
+    <div class="box-body">
+        <div class="col-md-12">
+            @component('layouts.errors')
+            @endcomponent
+        </div>
+        <div class="col-md-12">
+            <form class="form" role='form' method="POST" action="{{route('categoria.store')}}">
+                @csrf
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label>Nombre</label>
+                        <input class="form-control" type="text" required="required" maxlength="50" name="nombre">
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="form-group">
+                        <label>Descripción</label>
+                        <input class="form-control" type="text" placeholder="Descripción de la Categoría (opcional)" name="descripcion">
+                    </div>
+                </div>
+                <div class="col-md-12" style="margin-top: 20px !important">
+                    <div class="form-group">
+                        <button class="btn btn-success icon-btn pull-left" type="submit"><i class="fa fa-fw fa-lg fa-save"></i>Guardar</button>
+                        <button class="btn btn-info icon-btn pull-left" type="reset"><i class="fa fa-fw fa-lg fa-trash-o"></i>Limpiar</button>
+                        <a class="btn btn-danger icon-btn pull-left" href="{{route('categoria.index')}}"><i class="fa fa-fw fa-lg fa-times-circle"></i>Cancelar</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="modal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Información de Ayuda</h4>
+            </div>
+            <div class="modal-body">
+                <p>Agregue nuevas categorías, la descripción es opcional y el campo nombre debe llevar máximo 50 caracteres.</p>
+            </div>
+            <div class="modal-footer" style="background-color: #d2d6de !important; opacity: .65;">
+                <button type="button"  class="btn btn-block btn-danger btn-flat pull-right" data-dismiss="modal"> <i class="fa fa-reply"></i> Regresar</button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+@endsection
+@section('script')
+<script type="text/javascript">
+    $(document).ready(function () {
+
+    });
+</script>
+@endsection
